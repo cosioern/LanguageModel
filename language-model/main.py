7a0770e9e456a04b10a4fd069658c7db826da554
cosio <cosio@wisc.edu>
2026-07-04 01:08:23 -0500

    Custom Model & Sys Prompt to Microservice

--- language-model/main.py
+++ language-model/main.py
@@ -16,22 +16,29 @@ class GenerateRequest(BaseModel):
 
 # Load Module
 base_model = AutoModelForCausalLM.from_pretrained(
-    # model_name = "Qwen/Qwen2.5-3B-Instruct",
     "Qwen/Qwen2.5-3B-Instruct",
     torch_dtype=torch.float16,
     device_map="auto",
 )
-# model = PeftModel.from_pretrained(base_model, "./Adapters")
-# model = model.merge_and_unload()
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-3B-Instruct")
 
+model = PeftModel.from_pretrained(base_model, "./Adapters/Set_3")
+model = model.merge_and_unload()
+
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-3B-Instruct")
+system_prompt = (
+        "You are PIE, a real estate market analyst. "
+        "Provide concise, investment-focused commentary. "
+        "Base reasoning on supply, demand, interest rates, demographics, and valuation. "
+        "Avoid speculation and avoid making up specific local statistics. "
+        "Prioritize causal explanations and investment implications."
+)
 
 # Expose REST Endpoint, Receive the Prompt as JSON
 @app.post("/generate")
 def generate(req: GenerateRequest):
 
     messages = [
-    {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
+    {"role": "system", "content": system_prompt},
     {"role": "user", "content": req.prompt}
     ]
 
@@ -41,10 +48,10 @@ def generate(req: GenerateRequest):
     tokenize=False,
     add_generation_prompt=True
     )
-    model_inputs = tokenizer([text], return_tensors="pt").to(base_model.device)
+    model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
 
     # Run Generator
-    generated_ids = base_model.generate(
+    generated_ids = model.generate(
     **model_inputs,
     max_new_tokens=128, #512
     temperature=0.7,
