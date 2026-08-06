02a228184e85ee2d9965ffd193736d9511b87458
cosio <cosio@wisc.edu>
2026-08-05 19:23:14 -0500

    New Dataset Building Script + Error Handling

--- language-model/preprocessor.py
+++ language-model/preprocessor.py
@@ -1,10 +1,20 @@
 import pymupdf
 from pathlib import Path
-# from openai import OpenAI 2:20
+# from openai import OpenAI 4:55
 from google import genai
 from google.genai.errors import APIError, ClientError#, ServiceError
 import json
 import time
+import re
+
+# clean up text by removing hyphenation, newlines, and extra spaces
+def clean_text(text) -> str:
+    text = re.sub(r"(\w)-\s+(\w)", r"\1\2", text)
+    text = text.replace("\n", " ")
+    text = re.sub(r"([a-z])(\d)", r"\1 \2", text)
+    text = re.sub(r"(\d)([a-zA-Z])", r"\1 \2", text)
+    text = re.sub(r"\s+", " ", text)
+    return text.strip()
 
 # automatically loads API key from OPENAI_API_KEY environment variable
 # client = OpenAI()
@@ -39,6 +49,8 @@ for item in Path("textbooks").iterdir():
                 print(f"Failed to extract text from page {page_num} from {item.name}: {str(e)}")
                 continue
 
+            # header = ""
+            # paragraph = ""
             for block in page_dict["blocks"]:
                 if block["type"] != 0:
                     continue
@@ -47,73 +59,91 @@ for item in Path("textbooks").iterdir():
                         # font_size = span["size"]
                         # print("Font size: " + f"{font_size}" + " and text: " + span["text"])
                         if span["size"] == 20 or span["size"] == 18:
-                            if header and paragraph:
+                            if header and paragraph and len(paragraph) <= 3000:
+                                while True:
+                                    try:
+                                        paragraph = clean_text(paragraph)
+                                        interaction = client.interactions.create(
+                                            # model="gemini-3.6-flash",
+                                            model="gemini-3.1-flash-lite",
+                                            input=f"{Prompt}, Header: {header}, Text: {paragraph}",
+                                            generation_config={
+                                                "temperature": 0.3
+                                            }
+                                        )
+                                        if interaction.status == "completed" and interaction.output_text:
+                                            out.write(json.dumps({
+                                                "messages": [
+                                                    {"role": "user", "content": interaction.output_text},
+                                                    {"role": "assistant", "content": paragraph}
+                                                ]
+                                            }) + "\n")
+                                            time.sleep(5)
+                                            break
+                                            # print(f"Prompt: {interaction.output_text}\nResponse: {paragraph}\n\n")
+                                        else:
+                                            print("Failure")
+                                            time.sleep(5)
 
-                                try:
-                                    interaction = client.interactions.create(
-                                        # model="gemini-3.6-flash",
-                                        model="gemini-3.1-flash-lite",
-                                        input=f"{Prompt}, Header: {header}, Text: {paragraph}",
-                                        generation_config={
-                                            "temperature": 0.3
-                                        }
-                                    )
-                                    if interaction.status == "completed" and interaction.output_text:
-                                        print(f"Prompt: {interaction.output_text}\nResponse: {paragraph}\n\n")
-                                    else:
-                                        print("Failure")
-                                except ClientError as e:
-                                    print(f"Fix your code goofball: {e.code} - {e.message}")
-                                # except ServiceError as e:
-                                #     print(f"Google's fault: {e.code} - {e.message}")
-                                except APIError as e:
-                                    print(f"Something went wrong: {e.code} - {e.message}")
-                                time.sleep(5)
-                                # out.write(json.dumps({
-                                #     "messages": [
-                                #         {"role": "user", "content": interaction.output_text},
-                                #         {"role": "assistant", "content": paragraph}
-                                #     ]
-                                # }) + "\n")
-                                # print(header + "\n" + paragraph + "\n\n")
+                                    except ClientError as e:
+                                        if e.code == "429":
+                                            print(f"Rate Limit Exceeded. Waiting 30 seconds")
+                                            time.sleep(30)
+                                        else:
+                                            print(f"Fix your code goofball: {e.code} - {e.message}")
+                                    # except ServiceError as e:
+                                    #     print(f"Google's fault: {e.code} - {e.message}")
+                                    except APIError as e:
+                                        print(f"Something went wrong: {e.code} - {e.message}")
+                                        break
+
                             # set new header and reset paragraph
                             header = span["text"]
                             paragraph = ""
                         if span["size"] == 9.5:
                             paragraph += span["text"] + " "
 
-        if header and paragraph:
-
-            try:
-                interaction = client.interactions.create(
-                    # model="gemini-3.6-flash",
-                    model="gemini-3.1-flash-lite",
-                    input=f"{Prompt}, Header: {header}, Text: {paragraph}",
-                    generation_config={
-                        "temperature": 0.3
-                    }
-                )
-                if interaction.status == "completed" and interaction.output_text:
-                    print(f"Prompt: {interaction.output_text}\nResponse: {paragraph}\n\n")
-                else:
-                    print("Failure")
-            except ClientError as e:
-                print(f"Fix your code goofball: {e.code} - {e.message}")
-            # except ServiceError as e:
-            #     print(f"Google's fault: {e.code} - {e.message}")
-            except APIError as e:
-                print(f"Something went wrong: {e.code} - {e.message}")
-            time.sleep(5)
-            # out.write(json.dumps({
-            #     "messages": [
-            #         {"role": "user", "content": interaction.output_text},
-            #         {"role": "assistant", "content": paragraph}
-            #     ]
-            # }) + "\n")
+        if header and paragraph and len(paragraph) <= 3000:
+            while True:
+                try:
+                    paragraph = clean_text(paragraph)
+                    interaction = client.interactions.create(
+                        # model="gemini-3.6-flash",
+                        model="gemini-3.1-flash-lite",
+                        input=f"{Prompt}, Header: {header}, Text: {paragraph}",
+                        generation_config={
+                            "temperature": 0.3
+                        }
+                    )
+                    if interaction.status == "completed" and interaction.output_text:
+                        # print(f"Prompt: {interaction.output_text}\nResponse: {paragraph}\n\n")
+                        out.write(json.dumps({
+                            "messages": [
+                                {"role": "user", "content": interaction.output_text},
+                                {"role": "assistant", "content": paragraph}
+                            ]
+                        }) + "\n")
+                        time.sleep(5)
+                        break
+                    else:
+                        print("Failure")
+                        time.sleep(5)
+                except ClientError as e:
+                    if e.code == "429":
+                        print(f"Rate Limit Exceeded. Waiting 30 seconds")
+                        time.sleep(30)
+                    else:
+                        print(f"Fix your code goofball: {e.code} - {e.message}")
+                        break
+                # except ServiceError as e:
+                #     print(f"Google's fault: {e.code} - {e.message}")
+                except APIError as e:
+                    print(f"Something went wrong: {e.code} - {e.message}")
+                    break
 
     except Exception as e:
         print(f"Error: {str(e)}")
-        continue
+        # continue
     finally:
         if doc:
             doc.close()
