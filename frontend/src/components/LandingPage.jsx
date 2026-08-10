1272f393da1d82b4b6a68d91f11878c4546a993a
cosio <cosio@wisc.edu>
2026-08-09 20:10:54 -0500

    Message Rarity Closes #25

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -3,11 +3,31 @@ import Left from "./Left";
 import Right from "./Right";
 import "./LandingPage.css";
 
-const messages = ["Lay It On Me Mama!", 
-    "ChatGPT's Got Nothing On Me!", 
-    "Claude Who?", 
-    "I Think I Just Gained Consciousness...", 
-    "I Know Where You Live",];
+const messages = [
+    // {text: "'Sup Brah!", weight: 5},
+    {text: "Lay It On Me Mama!", weight: 10},
+    {text: "ChatGPT's Got Nothing On Me!", weight: 10},
+    {text: "Claude Who?", weight: 10},
+    {text: "I Think I Just Gained Consciousness...", weight: 10},
+    // "I Know Where You Live",
+    // {text: "Hello, Stranger", weight: 5},
+    {text: "Look Who Showed Up...", weight: 10}, 
+    // {text: "Fancy Seeing You Here", weight: 5},
+    // {text: "Against All Odds, We Meet", weight: 5},
+    // {text: "Nothing To See Here...", weight: 5},
+    {text: "Howdy!", weight: 10},
+    // {text: "At Last, Someone To Talk To", weight: 5},
+    // {text: "Please Remain Calm", weight: 5},
+    {text: "'Cause There's a Chance for Choices, and Its You I'm Choosin!", weight: 10},
+    {text: "Billie Jean Is Not My Lover!", weight: 10},
+    {text: "Don't Stop 'Til You Get Enough!", weight: 10},
+    {text: "He Got Monkey Finger, He Shoot Coca-Cola", weight: 10},
+    // {text: "And I Say, It's All Right", weight: 10},
+    {text: "Amiga, Hay Que Ver Como Es El Amor", weight: 1},
+    // "Pero Yo Te Busco En Cade Amanecer",
+    // "Tu, La Misma De Ayer!",
+    // "Ahora Te Puedes Marchar!",   
+];
 
 function LandingPage({onSubmit}) {
     const [prompt, setPrompt] = useState("");
@@ -39,8 +59,20 @@ function LandingPage({onSubmit}) {
     // load a random message on 
     useEffect(() => {
         function randomMessage() {
-            const rand = Math.floor(Math.random()*messages.length);
-            setGreeting(messages[rand]); 
+            let total = 0;
+            for (const msg of messages) {
+                total += msg.weight;
+            }
+            // const rand = Math.floor(Math.random()*messages.length);
+            let rand = Math.random() * total;
+            for (const msg of messages) {
+                rand -= msg.weight;
+                if (rand <= 0) {
+                    setGreeting(msg.text);
+                    return;
+                }
+            }
+            // setGreeting(messages[rand]); 
         }
         randomMessage();
     }, []);
